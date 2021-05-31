433b5ef17bea60a10af4481ed63f4e06b21ece10
Axel Barbé--Husson <47109555+AxelBarbeHusson@users.noreply.github.com>
2021-05-31 16:18:00 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,30 +1,35 @@
-sudo apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox -y
-sudo apt-get install --no-install-recommends chromium-browser -y
-cd /ect/xdg/openbox/
-sudo rm autostart.sh
-git clone 'https://github.com/AxelBarbeHusson/autostart.git'
-cd autostrat
-sudo mv autostrat.sh /openbox
-cd ..
-rm autostart
-chmod +x autostart.sh
-git clone 'https://github.com/AxelBarbeHusson/environement.git'
-cd environment
-sudo mv environment.sh /openbox
-cd ..
-rm environment
-chmod +x environment.sh
-cd ..
-cd ..
-cd ..
-touch ~/.bash_profile
-sudo echo "[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && startx -- -nocursor" >> ~/.bash_profile
-source ~/.bash_profile
-cd boot
-sed 's/#framebuffer_width=1280/#framebuffer_width=1920/g' config.txt
-sed 's/#framebuffer_height=720/#framebuffer_width=1080/g' config.txt
-sed 's/#hdmi_group=1/#hdmi_group=16/g' config.txt
-
-
-
-
+sudo apt-get install unclutter -y
+sudo apt-get install xdotool -y
+touch kiosk.sh
+sudo echo "#!/bin/bash" >> kiosk.sh
+sudo echo " " >> kiosk.sh
+sudo echo "xset s noblank" >> kiosk.sh
+sudo echo "xset s off" >> kiosk.sh
+sudo echo "xset -dpms" >> kiosk.sh
+sudo echo " " >> kiosk.sh
+sudo echo "unclutter -idle 1 -root &">> kiosk.sh
+sudo echo " " >> kiosk.sh
+sudo echo "/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://www.qeenta.com" >> kiosk.sh
+sudo echo " " >> kiosk.sh
+sudo echo "while true; do" >> kiosk.sh
+sudo echo "xdotool keydown ctrl+Tab; xdotool keyup ctrl+Tab;" >> kiosk.sh
+sudo echo "xdotool keydown ctrl+r; xdotool keyup ctrl+r;" >> kiosk.sh
+sudo echo "sleep 20" >> kiosk.sh
+sudo echo "done" >> kiosk.sh
+touch kiosk.service
+sudo echo "[Unit]" >> kiosk.service
+sudo echo "Description=Chromium Kiosk" >> kiosk.service
+sudo echo "Wants=graphical.target" >> kiosk.service
+sudo echo "After=graphical.target" >> kiosk.service
+sudo echo " " >> kiosk.service
+sudo echo "[Service]" >> kiosk.service
+sudo echo "Environment=DISPLAY=:0.0" >> kiosk.service
+sudo echo "Environment=XAUTHORITY=/hoùe/pi/kiosk.sh" >> kiosk.service
+sudo echo "Restart=on-abort" >> kiosk.service
+sudo echo "User=pi" >> kiosk.service
+sudo echo "Group=pi" >> kiosk.service
+sudo echo " " >> kiosk.service
+sudo echo "[Install]" >> kiosk.service
+sudo echo "WantedBy=graphical.target" >> kiosk.service
+sudo mv kiosk.service /lib/systemd/system
+sudo systemcl enable kiosk.service
