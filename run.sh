23df6f71bb4b6ff143de8d6ec3b690b67fedb86c
Axel Barbé--Husson <47109555+AxelBarbeHusson@users.noreply.github.com>
2021-05-31 12:04:51 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
-sudo apt-get update
-sudo apt install git -y
 sudo apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox -y
 sudo apt-get install --no-install-recommends chromium-browser -y
 cd /ect/xdg/openbox/
